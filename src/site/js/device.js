a4c7b8734046b4a95eadb6bffafa1fe38e6128c9
Jacob Ross <jacob.ross98@outlook.com>
2020-12-02 17:28:20 -0500

    made gauges responsive

--- src/site/js/device.js
+++ src/site/js/device.js
@@ -15,41 +15,59 @@ $(function() {
     if(!STRESS_TESTABLE.includes(group)){
         $("#stress-test-tab").parent().hide()
     }
+
     clicked = true;
-    $(document).ready(function(){
-      $("button").click(function(){
+    $("#startCPUStressTest").click(function(){
             if(clicked){
                 $(this).css('background-color', 'red');
-                document.getElementById("myButton2").innerHTML = "Stop"
+                document.getElementById("startCPUStressTest").innerHTML = "Stop"
                 clicked  = false;
                 api.send("cpu-Stress-Test-Start")
                   //Start stress test
-                } 
+            } 
             else {
                 $(this).css('background-color', 'green');  
-                document.getElementById("myButton2").innerHTML = "Start"
+                document.getElementById("startCPUStressTest").innerHTML = "Start"
                 clicked  = true;
                 api.send("cpu-Stress-Test-Stop")
                 //if stress test is running stop
-              }   
-        });
+            }   
     });
 
-    // EXAMPLE
+    let Gauges = []
     
-    var myCanvas = document.getElementById("myGauge");
-    var a = new Gauge({
-        canvas : myCanvas,
-        width_height : 300,
-        font : "30px Arial",
-        centerText : "Utilization:",
-        metricSymbol : "%"
-    }) 
+    $("[id$='Gauge']").each((index,ele)=>{
+        let name = ele.id.substring(0,ele.id.length-5)
+        let symbol = name == "temperature" ? "°C" : "%"
+        Gauges.push(new Gauge({
+            canvas : ele,
+            width_height : $(window).width()*(2/12) - 10,
+            font : ($(window).width()*(2/12))*.10 + "px Arial",
+            centerText : name.charAt(0).toUpperCase() + name.slice(1) + ":",
+            metricSymbol : symbol
+        }))
+    })
+
+    $(window).resize(function () {
+        let width = $(window).width()*(2/12) - 10;
+        Gauges.forEach((gauge)=>{
+            gauge.updateWidthHeight(width)
+            gauge.updateFont(width*.10 + "px Arial")
+        })
+    });
 
     api.send(`${group}-info-subscribe`, 1000)
     
     api.receive(`${group}-info`,(info)=>{
-        a.draw(info[0].load[0].Value.split(" ")[0])
+        Gauges.forEach((gauge)=>{
+            let type = gauge.getText();
+            if(type == "Temperature:"){
+                gauge.draw(info[0].temperature[info[0].temperature.length-1].Value.split(" ")[0])
+            }
+            else if(type == "Utilization:"){
+                gauge.draw(info[0].load[0].Value.split(" ")[0])
+            }
+        })
         $("#device-stats").empty()
         $("#device-stats").append(`Device : ${info[0].Text}<br>`)
     })
